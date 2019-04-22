3b48e6754fe3e9ddae3fb418da22d37aab220f92
alexanderpenn <lenz@wharton.upenn.edu>
2019-04-21 22:47:03 -0400

    added local settings

--- ucss/settings.py
+++ ucss/settings.py
@@ -130,3 +130,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
